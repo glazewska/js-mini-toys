8260949a556be7ecead3b7dfeb580518b4073e5f
glazewska <anka.glazewska@gmail.com>
2017-05-02 16:55:54 +0200

    Add files via upload

--- slider/app.js
+++ slider/app.js
@@ -1,28 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    var next = document.getElementById('nextPicture');
-    var prev = document.getElementById('prevPicture');
-    var pictures = document.querySelector('.slider').children;
-    // console.log(pictures);
+    const next = document.getElementById('nextPicture');
+    const prev = document.getElementById('prevPicture');
+    const pictures = document.querySelector('.slider').children;
 
-    var slideIndex = 0;
+    let slideIndex = 0;
 
     pictures[slideIndex].classList.add('visible');
 
-
     next.addEventListener('click', function() {
-        // console.log(next);
         if (slideIndex < pictures.length - 1) {
             pictures[slideIndex].classList.remove('visible');
             slideIndex++;
             pictures[slideIndex].classList.add('visible');
         }
-
-
     });
 
     prev.addEventListener('click', function() {
-        // console.log(prev);
         if (slideIndex > 0) {
             pictures[slideIndex].classList.remove('visible');
             slideIndex--;
